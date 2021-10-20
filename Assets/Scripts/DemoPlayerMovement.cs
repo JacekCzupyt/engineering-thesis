3117725068d43ebac6e2521a2230b97926e5821d
JacekCzupyt <kuba.czupyt@gmail.com>
2021-10-20 15:23:04 +0200

    Implement network manager prototype

--- Assets/Scripts/DemoPlayerMovement.cs
+++ Assets/Scripts/DemoPlayerMovement.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MLAPI;
 using UnityEngine;
 
-public class DemoPlayerMovement : MonoBehaviour {
+public class DemoPlayerMovement : NetworkBehaviour {
     private Rigidbody rb;
     public float acceleration = 2f;
 
@@ -12,17 +13,19 @@ public class DemoPlayerMovement : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        if(Input.GetKey(KeyCode.W))
-            rb.AddForce(transform.forward * acceleration);
-        if(Input.GetKey(KeyCode.S))
-            rb.AddForce(-transform.forward * acceleration);
-        if(Input.GetKey(KeyCode.D))
-            rb.AddForce(transform.right * acceleration);
-        if(Input.GetKey(KeyCode.A))
-            rb.AddForce(-transform.right * acceleration);
-        if(Input.GetKey(KeyCode.Space))
-            rb.AddForce(transform.up * acceleration);
-        if(Input.GetKey(KeyCode.LeftShift))
-            rb.AddForce(-transform.up * acceleration);
+        if (this.IsOwner) {
+            if (Input.GetKey(KeyCode.W))
+                rb.AddForce(transform.forward * acceleration);
+            if (Input.GetKey(KeyCode.S))
+                rb.AddForce(-transform.forward * acceleration);
+            if (Input.GetKey(KeyCode.D))
+                rb.AddForce(transform.right * acceleration);
+            if (Input.GetKey(KeyCode.A))
+                rb.AddForce(-transform.right * acceleration);
+            if (Input.GetKey(KeyCode.Space))
+                rb.AddForce(transform.up * acceleration);
+            if (Input.GetKey(KeyCode.LeftShift))
+                rb.AddForce(-transform.up * acceleration);
+        }
     }
 }
