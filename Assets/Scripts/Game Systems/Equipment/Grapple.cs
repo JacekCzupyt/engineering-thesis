6f74e2900117c37886cb009ba315e2cb2c55f287
JacekCzupyt <kuba.czupyt@gmail.com>
2021-11-27 15:12:34 +0100

    Add player ability input

--- Assets/Scripts/Game Systems/Equipment/Grapple.cs
+++ Assets/Scripts/Game Systems/Equipment/Grapple.cs
@@ -1,21 +1,42 @@
+using System.Diagnostics;
 using Input_Systems;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Debug = UnityEngine.Debug;
 
 namespace Game_Systems.Equipment {
     public class Grapple : MonoBehaviour {
         [SerializeField] private GameObject player;
         private CharacterInputManager input;
-    
-        // Start is called before the first frame update
+
+        public enum GrappleState {
+            Ready,
+            InFlight,
+            Attached,
+            Retracting,
+            Unavailable
+        }
+
+        public GrappleState State { get; set; }
+        
         void Start()
         {
             input = CharacterInputManager.Instance;
+            input.AbilityAction.started += Fire;
+            input.AbilityAction.canceled += Cancel;
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            Debug.Log(State);
+        }
+
+        void Fire(InputAction.CallbackContext context) {
+            State = GrappleState.InFlight;
+        }
+
+        void Cancel(InputAction.CallbackContext context) {
+            State = GrappleState.Ready;
         }
     }
 }
