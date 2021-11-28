91e6260e9c37f43b8edb3379dba960e9a15b5d1e
chapmanmd <mattdch99@outlook.com>
2021-11-28 15:11:36 +0100

    added lobby

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject playMenu;
     [SerializeField] private InputField playerNameInput;
     [SerializeField] private InputField passwordInput;
+    [SerializeField] private GameObject nameErrorText;
 
     public void Play(){
         initialMenu.SetActive(false);
@@ -28,9 +30,23 @@ public class MenuManager : MonoBehaviour
         initialMenu.SetActive(true);
     }
 
+    public void HostGame(){
+        if(NameValidation()) GameNetPortal.Instance.StartHost();
+    }
+
     public void ClientConnect(){
-        if(playerNameInput.text.Length > 0){
-            Debug.Log(playerNameInput.text);
+        if(NameValidation()) ClientGameNetPortal.Instance.StartClient();
+    }
+
+    public bool NameValidation(){
+        string playerName = playerNameInput.text;
+        if(playerName.Length <= 0)
+        {
+            nameErrorText.SetActive(true);
+            return false;
         }
+        nameErrorText.SetActive(false);
+        PlayerPrefs.SetString("PlayerName", playerName);
+        return true;
     }
 }
