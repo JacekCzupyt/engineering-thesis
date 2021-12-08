0f8bc4447f3f43ac111cc96660b50f757774d683
JacekCzupyt <kuba.czupyt@gmail.com>
2021-12-08 12:21:32 +0100

    Add unit test for object thrower

--- Assets/Tests/EditMode/ObjectThrowerTests.cs
+++ Assets/Tests/EditMode/ObjectThrowerTests.cs
@@ -1,30 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Game_Systems;
+using Game_Systems.Utility;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
 public class ObjectThrowerTests
 {
-    // A Test behaves as an ordinary method
     [Test]
-    public void ObjectThrowerTestsSimplePasses()
-    {
-        Assert.AreEqual(1, 1);
+    public void ReturnsStaticThrow() {
+        const ThrowType throwType = ThrowType.Static;
+        var throwMethod = ObjectThrower.Throw(throwType);
+        Assert.AreEqual((ObjectThrower.ThrowMethod)ObjectThrower.StaticThrow, throwMethod);
     }
-
-    [Test]
-    public void Fail() {
-        Assert.AreEqual(1, 0);
-    }
-
-    // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // // `yield return null;` to skip a frame.
-    // [UnityTest]
-    // public IEnumerator ObjectThrowerTestsWithEnumeratorPasses()
-    // {
-    //     // Use the Assert class to test conditions.
-    //     // Use yield to skip a frame.
-    //     yield return null;
-    // }
 }
