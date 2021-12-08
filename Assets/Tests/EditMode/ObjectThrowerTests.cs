83a3b52455a5e50a36bd0fff0cd68cae3a494cd9
JacekCzupyt <kuba.czupyt@gmail.com>
2021-12-08 12:26:26 +0100

    Add test namespace

--- Assets/Tests/EditMode/ObjectThrowerTests.cs
+++ Assets/Tests/EditMode/ObjectThrowerTests.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using Game_Systems;
 using Game_Systems.Utility;
 using NUnit.Framework;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.TestTools;
 
-public class ObjectThrowerTests
-{
-    [Test]
-    public void ReturnsStaticThrow() {
-        const ThrowType throwType = ThrowType.Static;
-        var throwMethod = ObjectThrower.Throw(throwType);
-        Assert.AreEqual((ObjectThrower.ThrowMethod)ObjectThrower.StaticThrow, throwMethod);
+namespace Tests.EditMode {
+    public class ObjectThrowerTests
+    {
+        [Test]
+        public void ReturnsStaticThrow() {
+            const ThrowType throwType = ThrowType.Static;
+            var throwMethod = ObjectThrower.Throw(throwType);
+            Assert.AreEqual((ObjectThrower.ThrowMethod)ObjectThrower.StaticThrow, throwMethod);
+        }
     }
 }
