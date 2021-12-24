ea2c10b4893976b6e1ba17d648e5e5dd515406c5
JacekCzupyt <kuba.czupyt@gmail.com>
2021-12-25 00:05:52 +0100

    Implement variable crosshair

---
+++ Assets/Scripts/UI/Game/Crosshair.cs
@@ -0,0 +1,32 @@
+using System;
+using Game_Systems.Utility;
+using UnityEngine;
+
+namespace UI.Game {
+    public class Crosshair : MonoBehaviour {
+        [SerializeField] private RectTransform crosshairCenter;
+        [SerializeField] private RectTransform crosshairLeft;
+        [SerializeField] private RectTransform crosshairRight;
+        [SerializeField] private RectTransform crosshairTop;
+        [SerializeField] private RectTransform crosshairBottom;
+
+        [SerializeField] private float minDistance;
+        [SerializeField] private Camera cam;
+
+        public void SetSpread(float distance) {
+            var trueDistance = distance + minDistance;
+            crosshairLeft.localPosition = new Vector3(-trueDistance, 0, 0);
+            crosshairRight.localPosition = new Vector3(trueDistance, 0, 0);
+            crosshairBottom.localPosition = new Vector3(0, -trueDistance, 0);
+            crosshairTop.localPosition = new Vector3(0, trueDistance, 0);
+        }
+
+        public void SetSpreadFromAngle(float angle) {
+            if (cam == null)
+                throw new NullReferenceException("SetSpreadFromAngle can't be used if camera is not assigned");
+
+            var screenSpacePoint = cam.ViewportToScreenPoint(new Vector3(angle / cam.fieldOfView, 0, 0)).x;
+            SetSpread(screenSpacePoint);
+        }
+    }
+}
